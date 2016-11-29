0b6fab88e63b2b96e8628665cff8974853cab11f
Chetan Sanghani <crsanghani@gmail.com>
2016-11-29 14:14:38 +0000

    refactored the tests to fit a different approach

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,12 +2,14 @@ require 'spec_helper'
 
 describe Account do
 
-  before :each do
-    @account = Account.new
-  end
+  let(:account) {Account.new}
 
   it 'can return a balance' do
-    expect(@account.balance).to eq 0
+    expect(account.balance).to eq 0
   end
 
+  it "can credit money in to an account" do
+    account.credit(5)
+    expect(account.balance).to eq 5
+  end
 end
--- spec/tansaction_spec.rb
+++ spec/tansaction_spec.rb
@@ -4,8 +4,4 @@ describe Transaction do
 
   let(:account) {Account.new}
 
-  it "can credit money in to an account" do
-    account.credit(5)
-    expect(account.balance).to eq 5
-  end
 end
