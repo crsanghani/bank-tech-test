f206d5733d8b361a732e95a1ca31d1c6712de98f
Chetan Sanghani <crsanghani@gmail.com>
2016-11-29 13:33:44 +0000

    first test written checking that account can return a balance

---
+++ spec/account_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Account do
+
+  before :each do
+    @account = Account.new
+  end
+
+  it 'can return a balance' do
+    expect(@account.balance)to eq(0)
+  end
+
+end
